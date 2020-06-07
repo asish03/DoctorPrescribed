ee2b4f454749c829e2bd987a90971d6bf9ce2732
bandana saw <64646557+bandanasaw@users.noreply.github.com>
2020-06-07 22:16:16 +0530

    bandana design updated

--- app/src/main/java/com/example/doctorpescribed/MainActivity.java
+++ app/src/main/java/com/example/doctorpescribed/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,5 +25,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
